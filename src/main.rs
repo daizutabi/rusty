27598b57e6499e113ee9c6a3bf115c6117434b5e
Daizu <daizutabi@gmail.com>
2024-05-06 01:41:26 +0000

    fin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+mod macros;
+mod app;
+mod commands;
+mod config;
+mod platform;
+
+use anyhow::Result;
+use clap::Parser;
+
+use crate::commands::cli::Cli;
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    app::set_global_verbosity(cli.verbose.log_level_filter());
+    app::set_global_config(config::load()?);
+
+    cli.exec()
 }
